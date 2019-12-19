41b9360cff71d5288c5402b36251edef42f005da
Mat Scales <mat@wibbly.org.uk>
2019-12-19 00:12:28 +0000

    Actually export the new Mat3 helper. Oops!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 This package uses semver for determining version numbers.
 
+## v1.1.1
+Actually exported the new mat3 helper.
+
 ## v1.1.0
 Added a 3x3 matrix, to be used for 2d transformations.
 
--- src/maths.ts
+++ src/maths.ts
@@ -2,12 +2,13 @@ import { v2, V2 } from './v2';
 import { v3, V3 } from './v3';
 import { v4, V4 } from './v4';
 import { quat, Quat } from './quaternion';
+import { mat3, Mat3 } from './mat3';
 import { mat4, Mat4 } from './mat4';
 import { clamp, randomPointInCircle, EPSILON } from './misc';
 
 
 export {
-  v2, v3, v4, quat, mat4,
-  V2, V3, V4, Quat, Mat4,
+  v2, v3, v4, quat, mat3, mat4,
+  V2, V3, V4, Quat, Mat3, Mat4,
   clamp, randomPointInCircle, EPSILON,
 };
